8d6d578202766e89613d38f878de4ea8862c1593
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-10-21 15:47:11 +0200

    Made comment an optional field in DiaryEntry

--- src/types.ts
+++ src/types.ts
@@ -2,10 +2,11 @@ export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy';
 
 export type Visibility = 'great' | 'good' | 'ok' | 'poor';
 
+// the question mark indicates that the field is optional
 export interface DiaryEntry {
     id: number;
     date: string;
     weather: Weather;
     visibility: Visibility;
-    comment: string;
+    comment?: string;
   }
